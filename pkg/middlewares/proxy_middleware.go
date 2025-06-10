24459db94bf40cecdd3313337c5677e8bb58b06c
sh5080 <prpn97@gmail.com>
2025-06-10 17:47:07 +0900

    feat: 내부 api외 비정상요청 차단

--- pkg/middlewares/proxy_middleware.go
+++ pkg/middlewares/proxy_middleware.go
@@ -42,17 +42,29 @@ func NewProxyMiddleware(serverService interfaces.ServerService) fiber.Handler {
 	return func(c *fiber.Ctx) error {
 		// [1] 요청 시작 및 초기화
 		requestId := utils.NewGenerate().GenerateRequestId()
-		utils.Infof("[%s] 새로운 프록시 요청 시작: %s %s", requestId, c.Method(), c.Path())
+		path := c.Path()
+
+		utils.Infof("[%s] 새로운 프록시 요청 시작: %s %s", requestId, c.Method(), path)
 
 		// [2] 내부 경로 체크
-		path := c.Path()
 		if pathUtil.IsInternalPath(path) {
 			utils.Infof("[%s] 내부 관리 경로 감지 (%s), 직접 처리", requestId, path)
 			return c.Next()
 		}
+
+		// [3] API 요청 검증
+		if !strings.HasPrefix(path, "/api") {
+			utils.Warnf("[%s] 비정상 요청 차단: %s %s from %s",
+				requestId, c.Method(), path, c.IP())
+			return c.Status(fiber.StatusForbidden).JSON(fiber.Map{
+				"success": false,
+				"message": "Forbidden",
+			})
+		}
+
 		utils.Infof("[%s] 내부 경로 아님, 프록시 처리 시작", requestId)
 
-		// [3] 최적의 서버 선택
+		// [4] 최적의 서버 선택
 		selectedServer := serverService.SelectOptimalServer()
 		if selectedServer == nil {
 			utils.Errorf("[%s] 선택된 서버가 없음", requestId)
@@ -63,16 +75,16 @@ func NewProxyMiddleware(serverService interfaces.ServerService) fiber.Handler {
 		}
 		utils.Infof("[%s] 선택된 서버: %s", requestId, selectedServer.ServerId)
 
-		// [4] 선택된 서버로 요청 시도
+		// [5] 선택된 서버로 요청 시도
 		err := tryServer(c, selectedServer, requestId)
 
-		// [5] 실패 시 서버리스로 장애 조치
+		// [6] 실패 시 서버리스로 장애 조치
 		if err != nil {
 			utils.Warnf("[%s] 기본 서버 실패, 서버리스로 장애 조치: %v", requestId, err)
 			fallbackServer := serverService.GetServerlessServer()
 			utils.Infof("[%s] 서버리스 서버 가져오기 완료: %+v", requestId, fallbackServer)
 
-			// [6] 서버리스로 재시도
+			// [7] 서버리스로 재시도
 			err = tryServer(c, fallbackServer, requestId)
 			if err != nil {
 				utils.Errorf("[%s] 서버리스 장애 조치 실패: %v", requestId, err)
@@ -83,7 +95,7 @@ func NewProxyMiddleware(serverService interfaces.ServerService) fiber.Handler {
 			}
 		}
 
-		// [7] 요청 완료 처리
+		// [8] 요청 완료 처리
 		serverService.FinishUsingServer(selectedServer.ServerId)
 		utils.Infof("[%s] 프록시 요청 완료", requestId)
 		return nil
