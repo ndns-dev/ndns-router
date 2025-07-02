4706969a8f8b866a935bdfb19576945225fcdb1b
sh5080 <prpn97@gmail.com>
2025-07-02 13:10:45 +0900

    feat: success, error response

---
+++ pkg/utils/response_util.go
@@ -0,0 +1,17 @@
+package utils
+
+import "github.com/gofiber/fiber/v2"
+
+func SendError(ctx *fiber.Ctx, status int, message string) error {
+	return ctx.Status(status).JSON(fiber.Map{
+		"message": message,
+		"success": false,
+	})
+}
+
+func SendSuccess(ctx *fiber.Ctx, data interface{}) error {
+	return ctx.JSON(fiber.Map{
+		"data":    data,
+		"success": true,
+	})
+}
