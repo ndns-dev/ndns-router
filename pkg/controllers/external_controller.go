2ea50ec979395e7f4931e3da00afa506d5eba1e9
sh5080 <prpn97@gmail.com>
2025-07-09 15:36:41 +0900

    fix: SseManager에서 상태 점검하는 것으로 통일함

--- pkg/controllers/external_controller.go
+++ pkg/controllers/external_controller.go
@@ -2,12 +2,10 @@ package controllers
 
 import (
 	"bufio"
-	"sync"
 	"time"
 
 	"github.com/gofiber/fiber/v2"
 	"github.com/sh5080/ndns-router/pkg/interfaces"
-	"github.com/sh5080/ndns-router/pkg/types"
 	"github.com/sh5080/ndns-router/pkg/types/dtos"
 	"github.com/sh5080/ndns-router/pkg/utils"
 	"github.com/valyala/fasthttp"
@@ -15,77 +13,27 @@ import (
 
 type ExternalController struct {
 	serverService interfaces.ServerService
-	clients       map[string]*types.ConnectionInfo
-	mu            sync.RWMutex
+	sseManager    *utils.SseManager
 }
 
-// NewExternalController는 새로운 ExternalController를 생성합니다
 func NewExternalController(serverService interfaces.ServerService) *ExternalController {
 	return &ExternalController{
 		serverService: serverService,
-		clients:       make(map[string]*types.ConnectionInfo),
+		sseManager:    utils.Global,
 	}
 }
 
-// GetActiveConnections는 현재 활성화된 SSE 연결들의 정보를 반환합니다
 func (c *ExternalController) GetActiveConnections(ctx *fiber.Ctx) error {
-	c.mu.RLock()
-	defer c.mu.RUnlock()
-
-	// 활성 연결 정보 수집
-	activeConnections := make([]types.Connection, 0)
-	for reqId, info := range c.clients {
-		connectionInfo := types.Connection{
-			ReqId:             reqId,
-			ConnectedAt:       info.ConnectedAt,
-			ConnectedDuration: time.Since(info.ConnectedAt),
-		}
-		activeConnections = append(activeConnections, connectionInfo)
-	}
-
+	activeConnections := c.sseManager.GetActiveConnections()
 	utils.Infof("[SSE] 활성 연결 조회 - 총 %d개의 연결", len(activeConnections))
 
-	activeConnectionsDto := dtos.ActiveConnections{
+	return utils.SendSuccessData(ctx, dtos.ActiveConnections{
 		TotalConnections: len(activeConnections),
 		Connections:      activeConnections,
-	}
-
-	return utils.SendSuccessData(ctx, activeConnectionsDto)
+	})
 }
 
-// RegisterMessageChannel은 특정 reqId에 대한 메시지 채널을 등록합니다
-func (c *ExternalController) RegisterMessageChannel(reqId string, msgChan chan string) {
-	c.mu.Lock()
-	defer c.mu.Unlock()
-
-	// 기존 채널이 있다면 닫기
-	if info, exists := c.clients[reqId]; exists {
-		close(info.Channel)
-		delete(c.clients, reqId)
-	}
-
-	c.clients[reqId] = &types.ConnectionInfo{
-		Channel:     msgChan,
-		ConnectedAt: time.Now(),
-	}
-	utils.Infof("[SSE] 메시지 채널 등록 완료 - reqId: %s", reqId)
-}
-
-// UnregisterMessageChannel은 특정 reqId의 메시지 채널을 제거합니다
-func (c *ExternalController) UnregisterMessageChannel(reqId string) {
-	c.mu.Lock()
-	defer c.mu.Unlock()
-
-	if info, exists := c.clients[reqId]; exists {
-		close(info.Channel)
-		delete(c.clients, reqId)
-		utils.Infof("[SSE] 메시지 채널 제거 완료 - reqId: %s", reqId)
-	}
-}
-
-// SseHandler는 외부 서버에서 데이터를 수신하는 핸들러입니다
 func (c *ExternalController) SseHandler(ctx *fiber.Ctx) error {
-	// reqId 파라미터 확인
 	reqId := ctx.Query("reqId")
 	if reqId == "" {
 		return utils.SendError(ctx, fiber.StatusBadRequest, "reqId 파라미터가 필요합니다")
@@ -93,33 +41,30 @@ func (c *ExternalController) SseHandler(ctx *fiber.Ctx) error {
 
 	messageChan := make(chan string, 10)
 
-	// SSE 헤더 설정
 	ctx.Set("Content-Type", "text/event-stream")
 	ctx.Set("Cache-Control", "no-cache")
 	ctx.Set("Connection", "keep-alive")
 
 	utils.Infof("[SSE] 새로운 연결 시작: %s", reqId)
 
-	// 메시지 채널 등록
-	c.RegisterMessageChannel(reqId, messageChan)
+	c.sseManager.Register(reqId, messageChan)
 
-	// 연결이 끊어졌을 때 정리
 	ctx.Context().SetConnectionClose()
 
-	// 스트림 작성기 설정
 	ctx.Context().Response.SetBodyStreamWriter(fasthttp.StreamWriter(func(w *bufio.Writer) {
 		defer func() {
 			utils.Infof("[SSE] 스트림 종료, 채널 정리 시작 - reqId: %s", reqId)
-			c.UnregisterMessageChannel(reqId)
+			c.sseManager.Deregister(reqId)
 			utils.Infof("[SSE] 스트림 종료, 채널 정리 완료 - reqId: %s", reqId)
 		}()
+
 		ssePayload := dtos.SsePayload{
 			Type: dtos.SseConnect,
 			Data: map[string]interface{}{
 				"message": "SSE 연결됨",
 			},
 		}
-		// 초기 연결 메시지 전송
+
 		if err := utils.SendSseEvent(w, &ssePayload); err != nil {
 			utils.Infof("[SSE] 초기 메시지 전송 실패 - reqId: %s, error: %v", reqId, err)
 			return
@@ -139,7 +84,7 @@ func (c *ExternalController) SseHandler(ctx *fiber.Ctx) error {
 				ssePayload := dtos.SsePayload{
 					Type: dtos.SseMessage,
 					Data: map[string]interface{}{
-						"message": msg,
+						"result": msg,
 					},
 				}
 				if err := utils.SendSseEvent(w, &ssePayload); err != nil {
@@ -147,6 +92,7 @@ func (c *ExternalController) SseHandler(ctx *fiber.Ctx) error {
 					return
 				}
 			case <-ticker.C:
+				utils.Infof("[SSE] 하트비트 전송 시도 - reqId: %s", reqId)
 				ssePayload := dtos.SsePayload{
 					Type: dtos.SseHeartbeat,
 					Data: map[string]interface{}{
@@ -164,9 +110,7 @@ func (c *ExternalController) SseHandler(ctx *fiber.Ctx) error {
 	return nil
 }
 
-// SendMessage는 특정 클라이언트에게 메시지를 전송하는 핸들러입니다
 func (c *ExternalController) SendMessage(ctx *fiber.Ctx) error {
-
 	req := new(dtos.MessageRequest)
 	if err := ctx.BodyParser(req); err != nil {
 		return utils.SendError(ctx, fiber.StatusBadRequest, "잘못된 요청 형식")
@@ -176,25 +120,8 @@ func (c *ExternalController) SendMessage(ctx *fiber.Ctx) error {
 		return utils.SendError(ctx, fiber.StatusBadRequest, "reqId와 message는 필수 값입니다")
 	}
 
-	c.mu.RLock()
-	defer c.mu.RUnlock()
-
 	utils.Infof("[SSE] Send 시도 - reqId: %s", req.ReqId)
-	if info, exists := c.clients[req.ReqId]; exists {
-		select {
-		case info.Channel <- req.Message:
-			utils.Infof("[SSE] 메시지 전송 성공 (reqId: %s)", req.ReqId)
-			return utils.SendSuccessData(ctx, types.Connection{
-				ReqId:             req.ReqId,
-				ConnectedAt:       info.ConnectedAt,
-				ConnectedDuration: time.Since(info.ConnectedAt),
-			})
-		default:
-			utils.Infof("[SSE] 메시지 전송 실패 (reqId: %s) - 채널이 가득 참", req.ReqId)
-			return utils.SendError(ctx, fiber.StatusServiceUnavailable, "메시지 전송 실패 - 채널이 가득 참")
-		}
-	} else {
-		utils.Infof("[WARN] [SSE] 채널을 찾을 수 없음 - reqId: %s", req.ReqId)
-		return utils.SendError(ctx, fiber.StatusNotFound, "존재하지 않는 reqId입니다")
-	}
+	c.sseManager.Send(req.ReqId, req.Message)
+
+	return utils.SendSuccessMessage(ctx, "메시지가 성공적으로 전송되었습니다")
 }
